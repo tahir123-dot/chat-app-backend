6195ae9064385c0f92ad09234205dc332168fe6a
Tahir Rashid <tahirbhai736333@gmail.com>
2026-02-28 08:19:29 +0500

    added new function getusersForSideBar online users

---
+++ controller/messageController.js
@@ -0,0 +1,31 @@
+import User from "../model/User.js";
+import Message from "../model/Message.js";
+
+// Get all users except the logged in User
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const filteredUsers = await User.find({ _id: { $ne: userId } }).select(
+      "-password",
+    );
+
+    const unseenMessages = {};
+    const promises = filteredUsers.map(async (user) => {
+      const messages = await Message.find({
+        senderId: user._id,
+        receiverId: userId,
+        seen: false,
+      });
+      if (messages.length > 0) {
+        unseenMessages[user._id] = messages.length;
+      }
+    });
+
+    await Promise.all(promises);
+    res.json({ success: true, users: filteredUsers, unseenMessages });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
