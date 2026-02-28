ef3d9f68bf823ae3a2ecb51f3f73a2b045f22301
Tahir Rashid <tahirbhai736333@gmail.com>
2026-02-28 08:32:15 +0500

    added new function in which message mark as seen using user id

--- controller/messageController.js
+++ controller/messageController.js
@@ -56,3 +56,16 @@ export const getMessages = async (req, res) => {
     res.json({ success: false, message: error.message });
   }
 };
+
+// api to mark message as seen using message id
+
+export const markMessageAsSeen = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await Message.findByIdAndUpdate(id, { seen: true });
+    res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
