6544d485e5e06c3c60b00afa4bb506889f7dfb45
Tahir Rashid <tahirbhai736333@gmail.com>
2026-02-28 09:41:52 +0500

    added send message controller and estabilsh socket.io server between sender and receiver

--- controller/messageController.js
+++ controller/messageController.js
@@ -1,5 +1,7 @@
 import User from "../model/User.js";
 import Message from "../model/Message.js";
+import cloudinary from "../lib/cloudinary.js";
+import { io, userSocketMap } from "../server.js";
 
 // Get all users except the logged in User
 
@@ -64,6 +66,40 @@ export const markMessageAsSeen = async (req, res) => {
     const { id } = req.params;
     await Message.findByIdAndUpdate(id, { seen: true });
     res.json({ success: true });
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Send Message to selected user
+
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const receiverId = req.params.id;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+
+    const newMessage = Message.create({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+
+    // Emit the new message to the receirver socket
+    const receiverSocketId = userSocketMap[receiverId];
+    if (receiverSocketId) {
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
+    res.json({ success: true, newMessage });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
--- server.js
+++ server.js
@@ -4,10 +4,43 @@ import cors from "cors";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
 import messageRouter from "./routes/messageRoutes.js";
+import { Server } from "socket.io";
 
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
+
+// Initialize socket.io server
+export const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+// Store online Users
+
+export const userSocketMap = {};
+
+// Socket.io connection handler
+
+io.on("connection", (socket) => {
+  const userId = socket.handshake.query.userId;
+  console.log("User Connected", userId);
+
+  if (userId) {
+    userSocketMap[userId] = socket.id;
+  }
+
+  // Emit online users to all connected clients
+
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+
+  socket.on("disconnect", () => {
+    console.log("User Disconnected", userId);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
+});
+
 const PORT = process.env.PORT || 5000;
 
 // Middleware
